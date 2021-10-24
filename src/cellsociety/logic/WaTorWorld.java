308fad75a1667fcaa03d726258797a829f2e55fc
Quentin MacFarlane <qtm@duke.edu>
2021-10-24 17:06:06 -0400

    started wator world

--- src/cellsociety/logic/WaTorWorld.java
+++ src/cellsociety/logic/WaTorWorld.java
@@ -7,14 +7,21 @@ import java.util.Map;
 
 public class WaTorWorld extends Simulation {
 
-    public WaTorWorld(Grid grid, Map<String, String> metadata) throws MissingSimulationArgumentError {
+    /**
+     * Constructs a new Simulation with a specified starting Grid and a Map of simulation-specific data
+     * values.
+     *
+     * @param grid     the starting grid of the simulation.
+     * @param metadata the user-specified values used by the simulation.
+     * @throws MissingSimulationArgumentError if the metadata is missing a required argument for the
+     *                                        simulation.
+     */
+    public WaTorWorld(Grid grid, Map<String, String> metadata)
+            throws MissingSimulationArgumentError {
         super(grid, metadata);
     }
 
-    /**
-     * the update function to be run every tick of the game
-     * checks each cell for its neighbors and changes the cell appropriately
-     */
+    @Override
     public void update() {
         for (int x = 0; x < getGrid().getWidth(); x++) {
             for (int y = 0; y < getGrid().getHeight(); y++) {
